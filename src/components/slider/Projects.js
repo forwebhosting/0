d54f5682542211062d1a517f830009ce8fe86334
mdthowfick1info <mdthowfick1info@gmail.com>
2023-09-27 21:44:51 +0530

    slider testing

--- src/components/slider/Projects.js
+++ src/components/slider/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -11,6 +11,8 @@ const YoutubeCloneLink = "https://youtubeclone-thowfickofficial.netlify.app/";
 const AmazonCloneLink = "https://amazonclone-thowfickofficial.netlify.app/";
 
 const Projects = () => {
+  const sliderRef = useRef(null);
+
   const sliderSettings = {
     dots: true,
     infinite: true,
@@ -19,54 +21,75 @@ const Projects = () => {
     slidesToScroll: 1,
     centerMode: true, // Enable center mode
     centerPadding: '60px', // Adjust the margin as needed
-    draggable: true, // Enable dragging
+    draggable: false, // Disable user dragging
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2, // Adjust for smaller screens
-          draggable: true, // Enable dragging for smaller screens if needed
         },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1, // Adjust for mobile screens
-          draggable: true, // Enable dragging for mobile screens if needed
         },
       },
     ],
   };
-  
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (sliderRef.current) {
+        sliderRef.current.slickNext(); // Programmatically advance to the next slide
+      }
+    }, 3000); // 1000 milliseconds (1 second) interval
+
+    return () => {
+      clearInterval(intervalId); // Clear the interval when the component unmounts
+    };
+  }, []);
 
   return (
     <section id="projects" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="grid justify-center items-center text-center">
         {/* Your title or description */}
       </div>
-      <Slider {...sliderSettings}>
+      <Slider {...sliderSettings }ref={sliderRef} className="relative">
+      <div className="project-card transition-transform transform scale-90 hover:scale-100">
         <ProjectsCard
           title="OTT CLONE"
           src={DisneyClone}
           websiteLink={DisneyCloneLink}
         />
+        </div>
+        <div className="project-card transition-transform transform scale-90 hover:scale-100">
         <ProjectsCard
           title="SOCIAL MEDIA CLONE"
           src={projectOne}
           websiteLink={YoutubeCloneLink}
         />
+         </div>
+         <div className="project-card transition-transform transform scale-90 hover:scale-100">
         <ProjectsCard
           title="E-commerce Website(Habeeb)"
           src={projectTwo}
           websiteLink={HabeebEcomLink}
         />
+        </div>
+        <div className="project-card transition-transform transform scale-90 hover:scale-100">
         <ProjectsCard
           title="Amazon Clone"
           src={AmazonClone}
           websiteLink={AmazonCloneLink}
         />
+        </div>
+        <div className="project-card transition-transform transform scale-90 hover:scale-100">
         <ProjectsCard title="NoBroker Clone" src={NobrokerClone} />
+        </div>
+        <div className="project-card transition-transform transform scale-90 hover:scale-100">
         <ProjectsCard title="Phising Tool" src={PhisingTool} />
+        </div>
       </Slider>
     </section>
   );
