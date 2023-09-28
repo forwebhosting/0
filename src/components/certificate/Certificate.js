4542029b5e4e60f48e393a5bf32791bf07b17874
mdthowfick1info <mdthowfick1info@gmail.com>
2023-09-28 11:04:06 +0530

    slider for certificatecard

--- src/components/certificate/Certificate.js
+++ src/components/certificate/Certificate.js
@@ -327,7 +327,7 @@ const Certificate = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     centerMode: true,
     centerPadding: '60px',
