57cae77b90e68c4cddf6a1bb7ef29e32d3760744
thowfick official <mdthowfickspeed@gmail.com>
2024-01-22 12:47:53 +0530

    building error fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Banner from "./components/banner/Banner";
 import Contact from "./components/contact/Contact";
 import Features from "./components/features/Features";
-import Footer from "./components/footer/Footer";
+// import Footer from "./components/footer/Footer";
 import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
 import ProjectsSlider from "./components/ProjectsSilder/Projects";
